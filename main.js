0625a43aaf5297a81796a1858a2ddda63e2046ac
Vaigarai14 <vaigaraithennarasu144@gmail.com>
2024-01-27 12:17:36 +0530

    fix some bugs in the task completed and uncompleted features

--- main.js
+++ main.js
@@ -7,10 +7,11 @@ var taskuncompleted = document.getElementById('task-uncomp-score')
 let count = []
 
 function createelement() {
-    if (inputel.value.length > 0) {
-        var li = document.createElement('li')
-        var litxt = document.createTextNode(inputel.value)
-        var img = document.createElement('img')
+    var li = document.createElement('li')
+    var litxt = document.createTextNode(inputel.value)
+    var img = document.createElement('img')
+
+    if (inputel.value.length > 0 && inputel.value.length > 3) {
         img.src = "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxZW0iIGhlaWdodD0iMWVtIiB2aWV3Qm94PSIwIDAgMjQgMjQiPjxwYXRoIGZpbGw9IiMwZjY0YTMiIGQ9Ik03IDIxcS0uODI1IDAtMS40MTItLjU4N1Q1IDE5VjZINFY0aDVWM2g2djFoNXYyaC0xdjEzcTAgLjgyNS0uNTg3IDEuNDEzVDE3IDIxek0xNyA2SDd2MTNoMTB6TTkgMTdoMlY4SDl6bTQgMGgyVjhoLTJ6TTcgNnYxM3oiLz48L3N2Zz4=";
         img.classList.add('imgstyle')
         li.append(litxt)
@@ -19,26 +20,45 @@ function createelement() {
         inputel.value = ''
         count.push(li)
         taskuncompleted.textContent = count.length;
+        console.log(count);
+
+    } else {
+        alert("⚠ Enter the value more than 3 characters !!!!!!!");
     }
+
 
     li.addEventListener('click', (event) => {
         if (event.target.localName === "li") {
             li.classList.toggle('checked');
-            // let taskcompletedcondtion = event.target.classList.value ?
-            //     taskcompleted.textContent++ && taskuncompleted.textContent-- : taskcompleted.textContent--
-            if (event.target.classList.value === "checked") {
+            let taskcondition = event.target.classList.value === "checked";
+            let task_null_condition = event.target.classList.value === "";
+
+            //! click event to increase and decrease the taskcompleted and uncompleted count
+
+            if (taskcondition) {
                 taskcompleted.textContent++
                 taskuncompleted.textContent--
-            } else if (event.target.classList.value == "") {
+            } else if (task_null_condition) {
                 taskcompleted.textContent--
                 taskuncompleted.textContent++
             }
-            // return taskcompletedcondtion;
         }
     })
 
-    img.addEventListener('click', () => {
+    img.addEventListener('click', (event) => {
         li.remove();
+        count.pop()
+        let removecondition = event.target.parentNode.className === "checked";
+        let removeconditionnull = event.target.parentNode.className === "";
+
+        //* terrnary condtion to delete count in the taskcompleted and uncompleted
+
+        let removeelements_checked =
+            removecondition ? taskcompleted.textContent-- : null;
+        let removeelements_unchecked =
+            removeconditionnull ? taskuncompleted.textContent-- : null
+
+        return removeelements_checked, removeelements_unchecked;
     })
 
 }
@@ -51,4 +71,5 @@ inputel.addEventListener('keyup', (event) => {
         createelement();
     }
 })
+
 
